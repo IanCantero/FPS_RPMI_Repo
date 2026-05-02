4d5245cbb5f05c732c80d2628b768e65dfc0644d
IanCantero <136346391+IanCantero@users.noreply.github.com>
2026-05-02 12:24:59 +0200

    PS_Munición

--- RPMI_3EVA/Assets/_FPS_RPMI/Scripts/PickUp.cs
+++ RPMI_3EVA/Assets/_FPS_RPMI/Scripts/PickUp.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
+
+    void Update()
+    {
+        gameObject.transform.Rotate(0, 50 * Time.deltaTime, 0); // Rota el objeto para hacerlo más visible
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
